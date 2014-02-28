3e9ea3e357b99ab50b96b3c2c513b87984ea1604
Nicowwwww <nicomeback@hotmail.fr>
2014-02-28 15:32:33 +0100

    methode score

--- src/com/supinfo/sokoban/Score.java
+++ src/com/supinfo/sokoban/Score.java
@@ -4,8 +4,25 @@ import java.util.Date;
 
 public class Score
 {
+    private long time_start;
+    private long time_stop;
+
     public Score() {
         long ts = (new Date()).getTime();
         System.out.println(ts);
+    }
+
+    public void start() {
+        long ts = (new Date()).getTime();
+        this.time_start = ts;
+    }
+
+    public void stop() {
+        long ts = (new Date()).getTime();
+        this.time_stop = ts;
+    }
+
+    public long calcul() {
+        return this.time_stop - this.time_start;
     }
 }
